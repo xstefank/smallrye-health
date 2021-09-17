cdfc08373e57098f4e381d43691526743bccbcff
xstefank <xstefank122@gmail.com>
2021-09-17 10:46:20 +0200

    Possible MP 5 (jakarta.*) handling in SmallRye

---
+++ mp-5/tck/src/main/java/io/smallrye/health/SmallRyeStartupServlet.java
@@ -0,0 +1,32 @@
+package io.smallrye.health;
+
+import java.io.IOException;
+
+import jakarta.inject.Inject;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(name = "SmallRyeStartupServlet", urlPatterns = "/health/started")
+public class SmallRyeStartupServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+
+        SmallRyeHealth health = reporter.getStartup();
+        if (health.isDown()) {
+            resp.setStatus(503);
+        }
+        try {
+            reporter.reportHealth(resp.getOutputStream(), health);
+        } catch (IOException ioe) {
+            HealthLogging.logger.error(ioe);
+            resp.setStatus(500);
+        }
+    }
+
+    @Inject
+    private SmallRyeHealthReporter reporter;
+}

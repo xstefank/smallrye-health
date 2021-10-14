bf98801b02b4558f8ed6fa57aa9d917ca133847c
xstefank <xstefank122@gmail.com>
2021-10-14 09:43:22 +0200

    Add jakarta transformed modules for sr-health

---
+++ jakarta/testsuite-jakarta/tck-jakarta/src/main/java/io/smallrye/health/JakartaSmallRyeHealthGroupServlet.java
@@ -0,0 +1,33 @@
+package io.smallrye.health;
+
+import java.io.IOException;
+
+import jakarta.inject.Inject;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(name = "JakartaSmallRyeHealthGroupServlet", urlPatterns = "/health/group/*")
+public class JakartaSmallRyeHealthGroupServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+
+        String pathInfo = req.getPathInfo();
+        SmallRyeHealth health = pathInfo != null ? reporter.getHealthGroup(pathInfo.substring(1)) : reporter.getHealthGroups();
+        if (health.isDown()) {
+            resp.setStatus(503);
+        }
+        try {
+            reporter.reportHealth(resp.getOutputStream(), health);
+        } catch (IOException ioe) {
+            HealthLogging.logger.error(ioe);
+            resp.setStatus(500);
+        }
+    }
+
+    @Inject
+    private SmallRyeHealthReporter reporter;
+}
---
+++ jakarta/testsuite-jakarta/tck-jakarta/src/main/java/io/smallrye/health/JakartaSmallRyeHealthServlet.java
@@ -0,0 +1,32 @@
+package io.smallrye.health;
+
+import java.io.IOException;
+
+import jakarta.inject.Inject;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(name = "SmallRyeHealthServlet", urlPatterns = "/health")
+public class JakartaSmallRyeHealthServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+
+        SmallRyeHealth health = reporter.getHealth();
+        if (health.isDown()) {
+            resp.setStatus(503);
+        }
+        try {
+            reporter.reportHealth(resp.getOutputStream(), health);
+        } catch (IOException ioe) {
+            HealthLogging.logger.error(ioe);
+            resp.setStatus(500);
+        }
+    }
+
+    @Inject
+    private SmallRyeHealthReporter reporter;
+}
